466d6b4f8429cff49c48c96e8f41c08110ff318c
Soumya Chidambar Rao Waddankeri <c.soumya.rao@gmail.com>
2021-11-07 18:59:58 -0600

    coor

---
+++ src/assets/js/flare-2.js
@@ -0,0 +1,236 @@
+{
+    "name": "Carousel",
+    "children": [
+     {
+      "name": "Master of Business Administration - General Management",
+      "children": [
+       {
+        "name": "Standard",
+        "children": [
+            {"name": "Fall-1-2018", 
+            "children": [
+                {"name": "FIN 53621-Financial Management-3", "size": 3812}
+               ]
+            },
+            {"name": "Fall-2-2018", "size": 3812}
+        ]
+       },
+       {
+        "name": "Expedited",
+        "children": [
+            {"name": "Fall-1-2018", 
+            "children": [
+                {"name": "MGMT 54616-Organizational Behavior in Administration-3", "size": 3938},
+                {"name": "FIN 53621-Financial Management-3", "size": 3812}
+               ]
+            },
+            {"name": "Fall-2-2018", "size": 3812}
+        ]
+       }
+      ]
+     },
+     {
+        "name": "Master of Business Administration - Human Resource",
+        "children": [
+         {
+          "name": "Standard",
+          "children": [
+           {"name": "Fall-1-2018", "size": 3938},
+           {"name": "Fall-2-2018", "size": 3812}
+          ]
+         },
+         {
+          "name": "Expedited",
+          "children": [
+              {"name": "Fall-1-2018", "size": 3938},
+              {"name": "Fall-2-2018", "size": 3812}
+          ]
+         }
+        ]
+       },
+       {
+        "name": "Master of Information Systems",
+        "children": [
+         {
+          "name": "Standard",
+          "children": [
+              {"name": "Fall-1-2018", 
+              "children": [
+                  {"name": "DBMS", "size": 3812}
+                 ]
+              },
+              {"name": "Fall-2-2018","children": [
+                {"name": "Java", "size": 3812}
+               ]}
+          ]
+         },
+         {
+          "name": "Expedited",
+          "children": [
+              {"name": "Fall-1-2018", 
+              "children": [
+                  {"name": "Computer Networks", "size": 3938},
+                  {"name": "Professionalism", "size": 3812}
+                 ]
+              },
+              {"name": "Fall-2-2018", "size": 3812}
+          ]
+         }
+        ]
+       },
+       {
+        "name": "Master of Business Administration - General Management",
+        "children": [
+         {
+          "name": "Standard",
+          "children": [
+              {"name": "Fall-1-2018", 
+              "children": [
+                  {"name": "FIN 53621-Financial Management-3", "size": 3812}
+                 ]
+              },
+              {"name": "Fall-2-2018", "size": 3812}
+          ]
+         },
+         {
+          "name": "Expedited",
+          "children": [
+              {"name": "Fall-1-2018", 
+              "children": [
+                  {"name": "MGMT 54616-Organizational Behavior in Administration-3", "size": 3938},
+                  {"name": "FIN 53621-Financial Management-3", "size": 3812}
+                 ]
+              },
+              {"name": "Fall-2-2018", "size": 3812}
+          ]
+         }
+        ]
+       },
+       {
+          "name": "Master of Business Administration - Human Resource",
+          "children": [
+           {
+            "name": "Standard",
+            "children": [
+             {"name": "Fall-1-2018", "size": 3938},
+             {"name": "Fall-2-2018", "size": 3812}
+            ]
+           },
+           {
+            "name": "Expedited",
+            "children": [
+                {"name": "Fall-1-2018", "size": 3938},
+                {"name": "Fall-2-2018", "size": 3812}
+            ]
+           }
+          ]
+         },
+         {
+          "name": "Master of Information Systems",
+          "children": [
+           {
+            "name": "Standard",
+            "children": [
+                {"name": "Fall-1-2018", 
+                "children": [
+                    {"name": "DBMS", "size": 3812}
+                   ]
+                },
+                {"name": "Fall-2-2018","children": [
+                  {"name": "Java", "size": 3812}
+                 ]}
+            ]
+           },
+           {
+            "name": "Expedited",
+            "children": [
+                {"name": "Fall-1-2018", 
+                "children": [
+                    {"name": "Computer Networks", "size": 3938},
+                    {"name": "Professionalism", "size": 3812}
+                   ]
+                },
+                {"name": "Fall-2-2018", "size": 3812}
+            ]
+           }
+          ]
+         },
+         {
+            "name": "Master of Business Administration - General Management",
+            "children": [
+             {
+              "name": "Standard",
+              "children": [
+                  {"name": "Fall-1-2018", 
+                  "children": [
+                      {"name": "FIN 53621-Financial Management-3", "size": 3812}
+                     ]
+                  },
+                  {"name": "Fall-2-2018", "size": 3812}
+              ]
+             },
+             {
+              "name": "Expedited",
+              "children": [
+                  {"name": "Fall-1-2018", 
+                  "children": [
+                      {"name": "MGMT 54616-Organizational Behavior in Administration-3", "size": 3938},
+                      {"name": "FIN 53621-Financial Management-3", "size": 3812}
+                     ]
+                  },
+                  {"name": "Fall-2-2018", "size": 3812}
+              ]
+             }
+            ]
+           },
+           {
+              "name": "Master of Business Administration - Human Resource",
+              "children": [
+               {
+                "name": "Standard",
+                "children": [
+                 {"name": "Fall-1-2018", "size": 3938},
+                 {"name": "Fall-2-2018", "size": 3812}
+                ]
+               },
+               {
+                "name": "Expedited",
+                "children": [
+                    {"name": "Fall-1-2018", "size": 3938},
+                    {"name": "Fall-2-2018", "size": 3812}
+                ]
+               }
+              ]
+             },
+             {
+              "name": "Master of Information Systems",
+              "children": [
+               {
+                "name": "Standard",
+                "children": [
+                    {"name": "Fall-1-2018", 
+                    "children": [
+                        {"name": "DBMS", "size": 3812}
+                       ]
+                    },
+                    {"name": "Fall-2-2018","children": [
+                      {"name": "Java", "size": 3812}
+                     ]}
+                ]
+               },
+               {
+                "name": "Expedited",
+                "children": [
+                    {"name": "Fall-1-2018", 
+                    "children": [
+                        {"name": "Computer Networks", "size": 3938},
+                        {"name": "Professionalism", "size": 3812}
+                       ]
+                    },
+                    {"name": "Fall-2-2018", "size": 3812}
+                ]
+               }
+              ]
+             }
+    ]
+   }
---
+++ src/qa.js
@@ -0,0 +1,1 @@
+export {default} from "./true.js";
